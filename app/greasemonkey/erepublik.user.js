2c16a7ee12efc53ab7abeeb2aee59e251bc116bf
Rystraum Gamonez <rystraum@gmail.com>
2012-06-07 02:39:09 +0800

    added greasemonkey user js

---
+++ app/greasemonkey/erepublik.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name        Erepublik
+// @namespace   erep
+// @include     http://economy.erepublik.com/en/*
+// @version     1
+// ==/UserScript==
+
+var console = unsafeWindow.console;
+
+var url = window.location;
+url = url.toString();
+var country, item_type, item_quality;
+
+var regex = /\/([0-9]+)/ig;
+var result = url.match(regex);
+
+if (result.length >= 3) {
+  country = result[0];
+  item_type = result[1];
+  item_quality = result[2];
+}
+
+console.log(country);
+console.log(item_type);
+console.log(item_quality);
+
+var table = document.getElementById("marketplace").getElementsByTagName("table")[0].getElementsByTagName("tbody")[0];
+
+if (table) {
+  table = table.innerHTML;
+  GM_xmlhttpRequest({
+    method: "POST",
+    url: "http://erep.webbyapp.com/receive",
+    data: "item[country]="+country+"&item[item_type]="+item_type+"&item[item_quality]="+item_quality+"&item[data]="+table,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  });
+} else {
+  console.log("don't see no tables");
+}
+
