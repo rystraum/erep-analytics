1db755b6e029fa9e7aa9483450853bc1d9157047
Rystraum Gamonez <rystraum@gmail.com>
2012-06-08 01:36:19 +0800

    added sanitize fields before_create filter

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ActiveRecord::Base
   attr_accessible :country, :data, :item_quality, :item_type
+
+  before_create :sanitize_fields
+
+  def sanitize_fields
+    self.country = self.country.split("/").last.to_i
+    self.item_quality = self.item_quality.split("/").last.to_i
+    self.item_type = self.item_type.split("/").last.to_i
+  end
 end
+
