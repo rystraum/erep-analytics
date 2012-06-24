0778db0467fb16cc89b4adc8b09ba5373142f0ed
Rystraum Gamonez <rystraum@gmail.com>
2012-06-24 13:54:39 +0800

    added item#parse_data

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,26 @@ class Item < ActiveRecord::Base
     self.item_quality = self.item_quality.split("/").last.to_i
     self.item_type = self.item_type.split("/").last.to_i
   end
+
+  def parse_data
+    html = data
+
+    raise BoughtMessage unless html.match(/successfully/).nil?
+
+    doc = Nokogiri::HTML(html)
+
+    parsed_data = []
+    doc.css('tr').each_with_index do |row, i|
+       provider_id = row.css('td.m_provider a').first.attribute_nodes.first.value.split("/").last
+       parsed_data[i] = {
+        provider: provider_id,
+        stock: row.css('td.m_stock').children.first.to_s.to_i,
+        price: (row.css('td.m_price > strong').children.first.to_s + row.css('td.m_price sup').children.first.to_s).to_f
+      }
+    end
+
+    parsed_data
+  end
+
 end
 
