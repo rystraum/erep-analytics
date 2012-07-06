f08d77eb01f165a5d622f722c4804af75069db28
Rystraum Gamonez <rystraum@gmail.com>
2012-07-06 21:29:29 +0800

    no layout if request is ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :get_countries
+  layout :get_layout
+
+  def get_layout
+    request.xhr? ? nil : 'application'
+  end
+
   def get_countries
     @countries = Country.all
     @mercs = Merchandise.find :all, order: "erep_item_code, quality"
