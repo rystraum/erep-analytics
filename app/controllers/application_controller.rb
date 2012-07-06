617421b06f38d92e8baa9fdd88a24b7f7caeb2c9
Rystraum Gamonez <rystraum@gmail.com>
2012-07-06 16:33:08 +0800

    before filter for sidebar entities

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :get_countries
+  def get_countries
+    @countries = Country.all
+    @mercs = Merchandise.find :all, order: "erep_item_code, quality"
+  end
 end
+
